300c3bd61d02b35a967b99cd8fd13e5ad83d4695
Sreehari <sreeharims2000@gmail.com>
2024-12-20 15:57:13 +0530

    cors default

--- src/server.js
+++ src/server.js
@@ -17,9 +17,6 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
 }))
 
 app.use('/api/auth', authRoutes);
